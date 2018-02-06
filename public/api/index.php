ca12d71a2c96b125dd6020eb09699b5a3bffa859
Jefferson Rylee <rylee.jeff385@gmail.com>
2018-02-07 02:50:18 +0800

    Added access control allow origin header Added a test controller

--- public/api/index.php
+++ public/api/index.php
@@ -1,4 +1,9 @@
 <?php
+// header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+// header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+
 /**
  * CodeIgniter
  *
